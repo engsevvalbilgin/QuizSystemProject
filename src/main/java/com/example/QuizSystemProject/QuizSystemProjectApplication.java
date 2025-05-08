d9fb6d5451793c7bc6d0998e96ba6884f9ae538e
Şevval Bilğin <eng.sevval.bilgin@gmail.com>
2025-05-08 19:57:43 +0300

    Springboot Projesine dönüşüm

---
+++ src/main/java/com/example/QuizSystemProject/QuizSystemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuizSystemProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizSystemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizSystemProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QuizSystemProject/QuizSystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuizSystemProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizSystemProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
